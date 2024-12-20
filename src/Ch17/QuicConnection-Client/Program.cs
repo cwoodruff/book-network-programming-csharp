4d41155cff7b6ec4be8bf16572a2d7a66b010d33
Chris Woodruff <cwoodruff@live.com>
2024-12-20 08:23:08 -0500

    quic demos checkin

--- src/Ch17/QuicConnection-Client/Program.cs
+++ src/Ch17/QuicConnection-Client/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net.Quic;
+using System.Net;
+using System.Net.Security;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+var connectionOptions = new QuicClientConnectionOptions
+{
+    RemoteEndPoint = new IPEndPoint(IPAddress.Loopback, 5001),
+    ClientAuthenticationOptions = new SslClientAuthenticationOptions
+    {
+        ApplicationProtocols = new List<SslApplicationProtocol> { SslApplicationProtocol.Http3 }
+    }
+};
+
+await using var connection = await QuicConnection.ConnectAsync(connectionOptions);
+Console.WriteLine("Connected to server using QuicConnection.");
+
+await using var stream = await connection.OpenOutboundStreamAsync(QuicStreamType.Bidirectional);
+await stream.WriteAsync(Encoding.UTF8.GetBytes("Hello from QuicConnection client!"));
+
+var buffer = new byte[1024];
+var bytesRead = await stream.ReadAsync(buffer);
+Console.WriteLine($"Server response: {Encoding.UTF8.GetString(buffer, 0, bytesRead)}");
+
+await connection.CloseAsync(0);
+
+
--- src/Ch17/QuicListener-Server/Program.cs
+++ src/Ch17/QuicListener-Server/Program.cs
@@ -1,3 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net;
+using System.Net.Quic;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Microsoft.AspNetCore.Server.Kestrel.Https.Internal;
 
-Console.WriteLine("Hello, World!");
+if (!QuicListener.IsSupported)
+{
+    Console.WriteLine("QUIC is not supported");
+    return;
+}
+
+var cert = CertificateLoader.LoadFromStoreCert("localhost", StoreName.My.ToString(), StoreLocation.CurrentUser, false);
+
+var serverConnectionOptions = new QuicServerConnectionOptions
+{
+    DefaultStreamErrorCode = 0x0A,
+    DefaultCloseErrorCode = 0x0B,
+    ServerAuthenticationOptions = new SslServerAuthenticationOptions
+    {
+        ApplicationProtocols = new List<SslApplicationProtocol> { SslApplicationProtocol.Http3 },
+        ServerCertificate = CertificateLoader.LoadFromStoreCert("localhost", StoreName.My.ToString(), StoreLocation.CurrentUser, false)
+    }
+};
+
+var listener = await QuicListener.ListenAsync(new QuicListenerOptions
+{
+    ListenEndPoint = new IPEndPoint(IPAddress.Loopback, 0),
+    ApplicationProtocols = new List<SslApplicationProtocol> { SslApplicationProtocol.Http3 },
+    ConnectionOptionsCallback = (_, _, _) => ValueTask.FromResult(serverConnectionOptions)
+});
+
+var isRunning = true;
+
+while (isRunning)
+{
+    var connection = await listener.AcceptConnectionAsync();
+    Console.WriteLine($"Connection established with {connection.RemoteEndPoint}");
+    _ = Task.Run(() => HandleConnectionAsync(connection));
+}
+
+await listener.DisposeAsync();
+
+
+static async Task HandleConnectionAsync(QuicConnection connection)
+{
+    await using var stream = await connection.AcceptInboundStreamAsync();
+    var buffer = new byte[1024];
+    var bytesRead = await stream.ReadAsync(buffer);
+    Console.WriteLine($"Received: {Encoding.UTF8.GetString(buffer, 0, bytesRead)}");
+
+    await stream.WriteAsync(Encoding.UTF8.GetBytes("Hello from server!"));
+    await connection.CloseAsync(0);
+}
