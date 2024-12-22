c63969acb0d6ff4a60ad474d0cf3e8a7c4437ad8
Chris Woodruff "Woody <cwoodruff@live.com>
2024-12-22 15:44:15 -0500

    fixed code and updated. Also deleted the QuicStream demos because they are not needed.

--- src/Ch17/QuicConnection-Client/Program.cs
+++ src/Ch17/QuicConnection-Client/Program.cs
@@ -1,25 +1,116 @@
-﻿using System.Net.Quic;
-using System.Net;
+﻿using System.Net;
+using System.Net.Quic;
 using System.Net.Security;
-using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Server.Kestrel.Https.Internal;
 
-var connectionOptions = new QuicClientConnectionOptions
+try
 {
-    RemoteEndPoint = new IPEndPoint(IPAddress.Loopback, 5001),
-    ClientAuthenticationOptions = new SslClientAuthenticationOptions
+    bool isRunning = true;
+    Console.WriteLine("Starting QUIC client...");
+
+    // First, check if QUIC is supported.
+    if (!QuicConnection.IsSupported)
     {
-        ApplicationProtocols = new List<SslApplicationProtocol> { SslApplicationProtocol.Http3 }
+        Console.WriteLine("QUIC is not supported, check for presence of libmsquic and support of TLS 1.3.");
+        return;
     }
-};
 
-await using var connection = await QuicConnection.ConnectAsync(connectionOptions);
-Console.WriteLine("Connected to server using QuicConnection.");
+    // Share configuration for each incoming connection.
+    // This represents the minimal configuration necessary.
+    var serverConnectionOptions = new QuicServerConnectionOptions
+    {
+        // Used to abort stream if it's not properly closed by the user.
+        // See https://www.rfc-editor.org/rfc/rfc9000#section-20.2
+        DefaultStreamErrorCode = 0x0A, // Protocol-dependent error code.
 
-await using var stream = await connection.OpenOutboundStreamAsync(QuicStreamType.Bidirectional);
-await stream.WriteAsync(Encoding.UTF8.GetBytes("Hello from QuicConnection client!"));
+        // Used to close the connection if it's not done by the user.
+        // See https://www.rfc-editor.org/rfc/rfc9000#section-20.2
+        DefaultCloseErrorCode = 0x0B, // Protocol-dependent error code.
 
-var buffer = new byte[1024];
-var bytesRead = await stream.ReadAsync(buffer);
-Console.WriteLine($"Server response: {Encoding.UTF8.GetString(buffer, 0, bytesRead)}");
+        // Same options as for server side SslStream.
+        ServerAuthenticationOptions = new SslServerAuthenticationOptions
+        {
+            // Specify the application protocols that the server supports. This list must be a subset of the protocols specified in QuicListenerOptions.ApplicationProtocols.
+            ApplicationProtocols = [new SslApplicationProtocol("protocol1")],
+            // Server certificate, it can also be provided via ServerCertificateContext or ServerCertificateSelectionCallback.
+            ServerCertificate = CertificateLoader.LoadFromStoreCert("localhost", StoreName.My.ToString(),
+                StoreLocation.CurrentUser, false)
+        }
+    };
+    // Initialize, configure the listener and start listening.
+    var listener = await QuicListener.ListenAsync(new QuicListenerOptions
+    {
+        // Define the endpoint on which the server will listen for incoming connections. The port number 0 can be replaced with any valid port number as needed.
+        ListenEndPoint = new IPEndPoint(IPAddress.Loopback, 0),
+        // List of all supported application protocols by this listener.
+        ApplicationProtocols = [new SslApplicationProtocol("protocol1")],
+        // Callback to provide options for the incoming connections, it gets called once per each connection.
+        ConnectionOptionsCallback = (_, _, _) => ValueTask.FromResult(serverConnectionOptions)
+    });
+    // This represents the minimal configuration necessary to open a connection.
+    var clientConnectionOptions = new QuicClientConnectionOptions
+    {
+        // End point of the server to connect to.
+        RemoteEndPoint = listener.LocalEndPoint,
 
-await connection.CloseAsync(0);
+        // Used to abort stream if it's not properly closed by the user.
+        // See https://www.rfc-editor.org/rfc/rfc9000#section-20.2
+        DefaultStreamErrorCode = 0x0A, // Protocol-dependent error code.
+
+        // Used to close the connection if it's not done by the user.
+        // See https://www.rfc-editor.org/rfc/rfc9000#section-20.2
+        DefaultCloseErrorCode = 0x0B, // Protocol-dependent error code.
+
+        // Optionally set limits for inbound streams.
+        MaxInboundUnidirectionalStreams = 10,
+        MaxInboundBidirectionalStreams = 100,
+
+        // Same options as for client side SslStream.
+        ClientAuthenticationOptions = new SslClientAuthenticationOptions
+        {
+            // List of supported application protocols.
+            ApplicationProtocols = [new SslApplicationProtocol("protocol1")],
+            // The name of the server the client is trying to connect to. Used for server certificate validation.
+            TargetHost = "localhost",
+            RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+        }
+    };
+
+    // Initialize, configure and connect to the server.
+    var connection = await QuicConnection.ConnectAsync(clientConnectionOptions);
+
+    Console.WriteLine($"Connected {connection.LocalEndPoint} --> {connection.RemoteEndPoint}");
+
+    // Open a bidirectional (can both read and write) outbound stream.
+    // Opening a stream reserves it but does not notify the peer or send any data. If you don't send data, the peer
+    // won't be informed about the stream, which can cause AcceptInboundStreamAsync() to hang. To avoid this, ensure
+    // you send data on the stream to properly initiate communication.
+    var outgoingStream = await connection.OpenOutboundStreamAsync(QuicStreamType.Bidirectional);
+
+    // Work with the outgoing stream ...
+
+    // To accept any stream on a client connection, at least one of MaxInboundBidirectionalStreams or MaxInboundUnidirectionalStreams of QuicConnectionOptions must be set.
+    while (isRunning)
+    {
+        // Accept an inbound stream.
+        var incomingStream = await connection.AcceptInboundStreamAsync();
+
+        // Work with the incoming stream ...
+        byte[] buffer = new byte[1024];
+        await incomingStream.ReadExactlyAsync(buffer);
+        string recoveredString = System.Text.Encoding.UTF8.GetString(buffer);
+
+        Console.WriteLine(recoveredString + "Received: " + DateTime.UtcNow.ToString());
+    }
+
+    // Close the connection with the custom code.
+    await connection.CloseAsync(0x0C);
+
+    // Dispose the connection.
+    await connection.DisposeAsync();
+}
+catch (Exception ex)
+{
+    throw new Exception(ex.Message, ex);
+}
--- src/Ch17/QuicListener-Server/Program.cs
+++ src/Ch17/QuicListener-Server/Program.cs
@@ -5,51 +5,113 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using Microsoft.AspNetCore.Server.Kestrel.Https.Internal;
 
-if (!QuicListener.IsSupported)
+try
 {
-    Console.WriteLine("QUIC is not supported");
-    return;
-}
+    bool isRunning = true;
+    Console.WriteLine("Starting QUIC client...");
 
-var cert = CertificateLoader.LoadFromStoreCert("localhost", StoreName.My.ToString(), StoreLocation.CurrentUser, false);
-
-var serverConnectionOptions = new QuicServerConnectionOptions
-{
-    DefaultStreamErrorCode = 0x0A,
-    DefaultCloseErrorCode = 0x0B,
-    ServerAuthenticationOptions = new SslServerAuthenticationOptions
+    // First, check if QUIC is supported.
+    if (!QuicConnection.IsSupported)
     {
-        ApplicationProtocols = new List<SslApplicationProtocol> { SslApplicationProtocol.Http3 },
-        ServerCertificate = CertificateLoader.LoadFromStoreCert("localhost", StoreName.My.ToString(), StoreLocation.CurrentUser, false)
+        Console.WriteLine("QUIC is not supported, check for presence of libmsquic and support of TLS 1.3.");
+        return;
     }
-};
 
-var listener = await QuicListener.ListenAsync(new QuicListenerOptions
-{
-    ListenEndPoint = new IPEndPoint(IPAddress.Loopback, 0),
-    ApplicationProtocols = new List<SslApplicationProtocol> { SslApplicationProtocol.Http3 },
-    ConnectionOptionsCallback = (_, _, _) => ValueTask.FromResult(serverConnectionOptions)
-});
+    // Share configuration for each incoming connection.
+    // This represents the minimal configuration necessary.
+    var serverConnectionOptions = new QuicServerConnectionOptions
+    {
+        // Used to abort stream if it's not properly closed by the user.
+        // See https://www.rfc-editor.org/rfc/rfc9000#section-20.2
+        DefaultStreamErrorCode = 0x0A, // Protocol-dependent error code.
 
-var isRunning = true;
+        // Used to close the connection if it's not done by the user.
+        // See https://www.rfc-editor.org/rfc/rfc9000#section-20.2
+        DefaultCloseErrorCode = 0x0B, // Protocol-dependent error code.
 
-while (isRunning)
-{
-    var connection = await listener.AcceptConnectionAsync();
-    Console.WriteLine($"Connection established with {connection.RemoteEndPoint}");
-    _ = Task.Run(() => HandleConnectionAsync(connection));
+        // Same options as for server side SslStream.
+        ServerAuthenticationOptions = new SslServerAuthenticationOptions
+        {
+            // Specify the application protocols that the server supports. This list must be a subset of the protocols specified in QuicListenerOptions.ApplicationProtocols.
+            ApplicationProtocols = [new SslApplicationProtocol("protocol1")],
+            // Server certificate, it can also be provided via ServerCertificateContext or ServerCertificateSelectionCallback.
+            ServerCertificate = CertificateLoader.LoadFromStoreCert("localhost", StoreName.My.ToString(),
+                StoreLocation.CurrentUser, false)
+        }
+    };
+    // Initialize, configure the listener and start listening.
+    var listener = await QuicListener.ListenAsync(new QuicListenerOptions
+    {
+        // Define the endpoint on which the server will listen for incoming connections. The port number 0 can be replaced with any valid port number as needed.
+        ListenEndPoint = new IPEndPoint(IPAddress.Loopback, 0),
+        // List of all supported application protocols by this listener.
+        ApplicationProtocols = [new SslApplicationProtocol("protocol1")],
+        // Callback to provide options for the incoming connections, it gets called once per each connection.
+        ConnectionOptionsCallback = (_, _, _) => ValueTask.FromResult(serverConnectionOptions)
+    });
+    // This represents the minimal configuration necessary to open a connection.
+    var clientConnectionOptions = new QuicClientConnectionOptions
+    {
+        // End point of the server to connect to.
+        RemoteEndPoint = listener.LocalEndPoint,
+
+        // Used to abort stream if it's not properly closed by the user.
+        // See https://www.rfc-editor.org/rfc/rfc9000#section-20.2
+        DefaultStreamErrorCode = 0x0A, // Protocol-dependent error code.
+
+        // Used to close the connection if it's not done by the user.
+        // See https://www.rfc-editor.org/rfc/rfc9000#section-20.2
+        DefaultCloseErrorCode = 0x0B, // Protocol-dependent error code.
+
+        // Optionally set limits for inbound streams.
+        MaxInboundUnidirectionalStreams = 10,
+        MaxInboundBidirectionalStreams = 100,
+
+        // Same options as for client side SslStream.
+        ClientAuthenticationOptions = new SslClientAuthenticationOptions
+        {
+            // List of supported application protocols.
+            ApplicationProtocols = [new SslApplicationProtocol("protocol1")],
+            // The name of the server the client is trying to connect to. Used for server certificate validation.
+            TargetHost = "localhost",
+            RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+        }
+    };
+
+    // Initialize, configure and connect to the server.
+    var connection = await QuicConnection.ConnectAsync(clientConnectionOptions);
+
+    Console.WriteLine($"Connected {connection.LocalEndPoint} --> {connection.RemoteEndPoint}");
+
+    // Open a bidirectional (can both read and write) outbound stream.
+    // Opening a stream reserves it but does not notify the peer or send any data. If you don't send data, the peer
+    // won't be informed about the stream, which can cause AcceptInboundStreamAsync() to hang. To avoid this, ensure
+    // you send data on the stream to properly initiate communication.
+    var outgoingStream = await connection.OpenOutboundStreamAsync(QuicStreamType.Bidirectional);
+
+    // Work with the outgoing stream ...
+
+    // To accept any stream on a client connection, at least one of MaxInboundBidirectionalStreams or MaxInboundUnidirectionalStreams of QuicConnectionOptions must be set.
+    while (isRunning)
+    {
+        // Accept an inbound stream.
+        var incomingStream = await connection.AcceptInboundStreamAsync();
+
+        // Work with the incoming stream ...
+        byte[] buffer = new byte[1024];
+        await incomingStream.ReadExactlyAsync(buffer);
+        string recoveredString = System.Text.Encoding.UTF8.GetString(buffer);
+
+        Console.WriteLine(recoveredString + "Received: " + DateTime.UtcNow.ToString());
+    }
+
+    // Close the connection with the custom code.
+    await connection.CloseAsync(0x0C);
+
+    // Dispose the connection.
+    await connection.DisposeAsync();
 }
-
-await listener.DisposeAsync();
-
-
-static async Task HandleConnectionAsync(QuicConnection connection)
+catch (Exception ex)
 {
-    await using var stream = await connection.AcceptInboundStreamAsync();
-    var buffer = new byte[1024];
-    var bytesRead = await stream.ReadAsync(buffer);
-    Console.WriteLine($"Received: {Encoding.UTF8.GetString(buffer, 0, bytesRead)}");
-
-    await stream.WriteAsync(Encoding.UTF8.GetBytes("Hello from server!"));
-    await connection.CloseAsync(0);
+    throw new Exception(ex.Message, ex);
 }
